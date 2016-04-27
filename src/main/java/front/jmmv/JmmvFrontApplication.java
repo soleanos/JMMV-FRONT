2306f92536231a8ee6a774db15b8d8aba0223ce4
tomphi <thomas.philippe5555@gmail.com>
2016-04-27 14:07:53 +0200

    FIRST COMMIT tomphi

---
+++ src/main/java/front/jmmv/JmmvFrontApplication.java
@@ -0,0 +1,12 @@
+package front.jmmv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmmvFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmmvFrontApplication.class, args);
+	}
+}
